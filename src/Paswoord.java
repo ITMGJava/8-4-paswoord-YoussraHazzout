b410e6c72450f00e99c1fa71c947bcc3a05c8570
YoussraHazzout <YoussraHazzout@gmail.com>
2020-10-01 12:34:16 +0200

    gelukt maar nog niet mt ***

--- src/Paswoord.java
+++ src/Paswoord.java
@@ -7,36 +7,38 @@ public class Paswoord {
         Scanner s = new Scanner(System.in);
 
         System.out.println("Welkom op het inlog portaal, kies een gebruikersnaam:");
-var gebruikersnaam = s.nextLine();
-        System.out.println("Uw gebruikersnaam is:" +gebruikersnaam+ " Deze is onthouden.");
+        var gebruikersnaam = s.nextLine();
+        System.out.println("Uw gebruikersnaam is:" + gebruikersnaam + " Deze is onthouden.");
         System.out.println();
         System.out.println(" Voer een wachtwoord in: ");
-      /*  var c = System.console();
-        var pw = c.readPassword();
+      //var c = System.console();
+        //var pw = c.readPassword();
 
-       */
         var wachtwoord = s.nextLine();
         boolean check = true;
+
         System.out.println("Uw account is aangemaakt en ingesteld.");
         System.out.println();
+        var input = "";
         do {
             {
                 System.out.println("Uw gebruikernaam is:" + gebruikersnaam);
                 System.out.println("Voer uw wachtwoord in:");
-                var input = s.nextLine();
-                if (input == wachtwoord) {
-                    check = true;
+                input = s.nextLine();
+                if (input.equals(wachtwoord)) {
+                    input = s.nextLine();
                     System.out.println("U bent ingelogd.");
+                    check = true;
 
-            } else if (input != wachtwoord) {
+                } else if (!input.equals(wachtwoord)) {
                     System.out.println("U heeft een foutief wachtwoord ingevoerd, probeer het opnieuw.");
-                    input=s.nextLine();
+                    input = s.nextLine();
                     check = false;
 
                 }
 
             }
-        } while(!check);
+        } while (!input.equals(wachtwoord));
+    }
 }
-        }
 
